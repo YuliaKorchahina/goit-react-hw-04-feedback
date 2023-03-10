0c72f54e1a216954dd1d4d18d3c5ecf5737c158d
YuliaKorchahina <ruda_nokia@ukr.net>
2023-03-10 16:06:53 +0200

    hook

---
+++ src/components/Container.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 500px;
+  padding: 15px; 
+  background-color: #DE9BB8;
+  margin: 0 auto;
+  border-radius: 40px
+`;
